40dbdb380b65826efc239f45377af638e1f4b05b
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2026-05-07 16:18:27 +0200

    Delegate change password functionality to PATCH /account/<id>

--- backend/src/main/java/io/github/siniarski/viruni/controller/AuthController.java
+++ backend/src/main/java/io/github/siniarski/viruni/controller/AuthController.java
@@ -2,30 +2,23 @@ package io.github.siniarski.viruni.controller;
 
 import io.github.siniarski.viruni.RestResponse;
 import io.github.siniarski.viruni.security.auth.AccountPrinciple;
-import io.github.siniarski.viruni.dto.request.ChangePasswordRequest;
 import io.github.siniarski.viruni.dto.response.SignInResponse;
 import io.github.siniarski.viruni.dto.request.SignInRequest;
 import io.github.siniarski.viruni.dto.request.SignUpRequest;
 import io.github.siniarski.viruni.model.Account;
-import io.github.siniarski.viruni.model.AccountRole;
 import io.github.siniarski.viruni.repository.AccountRepository;
 import io.github.siniarski.viruni.security.jwt.JwtDetails;
 import io.github.siniarski.viruni.security.jwt.JwtProvider;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
-import java.time.Instant;
 
 @RestController
 public class AuthController {
@@ -80,28 +73,7 @@ public class AuthController {
         return RestResponse.created(account);
     }
 
-    @PostMapping("/account/password")
-    public ResponseEntity<?> changePassword(Principal principal,
-                                            @RequestBody @Valid ChangePasswordRequest form) {
-        Account account = accountRepository.findByUsername(principal.getName()).orElse(null);
-        if(account == null) return RestResponse.notFound("account not found");
 
-        account.setPassword(passwordEncoder.encode(form.getPassword()));
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-
-    @PostMapping("/account/{id}/password")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> changePasswordOfAccount(Principal principal,
-                                                     @PathVariable long id,
-                                                     @RequestBody @Valid ChangePasswordRequest form) {
-        Account account = accountRepository.findById(id).orElse(null);
-        if(account == null) return RestResponse.notFound();
-
-        account.setPassword(passwordEncoder.encode(form.getPassword()));
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 
     @PostMapping("/auth/renew")
     public ResponseEntity<SignInResponse> renewToken(Authentication auth) {
@@ -112,14 +84,5 @@ public class AuthController {
         AccountPrinciple userDetails = (AccountPrinciple) auth.getPrincipal();
 
         return RestResponse.ok(new SignInResponse(jwtDetails, userDetails.getUsername(), userDetails.getId(), userDetails.getAuthorities()));
-    }
-
-    @PostMapping("/account")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Account> createAccount(@RequestBody @Valid Account account) {
-        account.setId(0);
-        account.setPassword(passwordEncoder.encode(account.getPassword()));
-        accountRepository.save(account);
-        return RestResponse.ok(account);
     }
 }
--- backend/src/main/java/io/github/siniarski/viruni/dto/request/UpdateAccountRequest.java
+++ backend/src/main/java/io/github/siniarski/viruni/dto/request/UpdateAccountRequest.java
@@ -3,6 +3,7 @@ package io.github.siniarski.viruni.dto.request;
 public class UpdateAccountRequest {
     private String firstname;
     private String lastname;
+    private String password;
 
     public String getLastname() {
         return lastname;
@@ -11,4 +12,6 @@ public class UpdateAccountRequest {
     public String getFirstname() {
         return firstname;
     }
+
+    public String getPassword() {return password;}
 }
