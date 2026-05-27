3aef6def69796bd31d4bda3259bc80715708cc9b
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2026-05-27 12:40:17 +0200

    Create integration tests for SubjectController

---
+++ backend/src/test/java/io/github/siniarski/viruni/test/controller/SubjectControllerTest.java
@@ -0,0 +1,244 @@
+package io.github.siniarski.viruni.test.controller;
+
+import io.github.siniarski.viruni.dto.request.CreateSubjectRequest;
+import io.github.siniarski.viruni.dto.request.UpdateSubjectRequest;
+import io.github.siniarski.viruni.dto.response.PagedResponse;
+import io.github.siniarski.viruni.model.Subject;
+import io.github.siniarski.viruni.repository.AccountRepository;
+import io.github.siniarski.viruni.repository.SubjectRepository;
+import io.github.siniarski.viruni.test.BaseIntegrationTest;
+import io.github.siniarski.viruni.test.ContainerizedConfiguration;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Tag("integration")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(ContainerizedConfiguration.class)
+public class SubjectControllerTest extends BaseIntegrationTest {
+    @LocalServerPort
+    private Integer serverPort;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssured.baseURI = "http://localhost:" + serverPort;
+    }
+
+    @AfterEach
+    void afterEach() {
+        AUTH_RESPONSES.clear();
+    }
+
+    static Stream<Arguments> accountsWithoutSubjectManagmentPermissionsStream() {
+        return Stream.of(
+                Arguments.of("aliciaprice", "magics"),
+                Arguments.of("charlesangelica", "secret")
+        );
+    }
+
+    @Test
+    @DisplayName("GET /subject forbids unauthenticated users")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void getSubject_forbidsUnauthenticated() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().ifValidationFails()
+                .get("/subject")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    @DisplayName("GET /subject lists subjects")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void getSubject_listing() {
+        var resp = givenAuthenticatedAs("admin", "admin")
+                .contentType(ContentType.JSON)
+                .log().ifValidationFails()
+                .get("/subject")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(new TypeRef<PagedResponse<Subject>>(){});
+
+        assertThat(resp.getTotalElements()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("GET /subject?participant=<id> filters by participants")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void getSubject_filtersByParticipant() {
+        var participant = accountRepository.findById(2L).orElseThrow();
+
+        var resp = givenAuthenticatedAs("charlesangelica", "secret")
+                .contentType(ContentType.JSON)
+                .log().ifValidationFails()
+                .queryParam("participant", participant.getId())
+                .get("/subject")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(new TypeRef<PagedResponse<Subject>>(){});
+
+        assertThat(resp.getTotalElements()).isEqualTo(1);
+        assertThat(resp.getContent().stream().map(Subject::getId))
+                .containsExactly(2L);
+    }
+
+    @ParameterizedTest
+    @DisplayName("POST /subject forbids creating new subjects by teachers and regular users")
+    @MethodSource("accountsWithoutSubjectManagmentPermissionsStream")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void postSubject_forbidsTeachersAndRegularUsers(String username, String password) {
+        givenAuthenticatedAs(username, password)
+                .contentType(ContentType.JSON)
+                .body(new CreateSubjectRequest())
+                .log().ifValidationFails()
+                .post("/subject")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(403);
+    }
+
+    @ParameterizedTest
+    @DisplayName("DELETE /subject/<id> forbids deleting subjects by teachers and regular users")
+    @MethodSource("accountsWithoutSubjectManagmentPermissionsStream")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void deleteSubject_forbidsTeachersAndRegularUsers(String username, String password) {
+        // Given subject of id 1 exists
+        var subject = subjectRepository.findById(1L).orElseThrow();
+
+        givenAuthenticatedAs(username, password)
+                .contentType(ContentType.JSON)
+                .log().ifValidationFails()
+                .delete("/subject/"+subject.getId())
+                .then()
+                .log().ifValidationFails()
+                .statusCode(403);
+    }
+
+    @Test
+    @DisplayName("POST /subject permits creating new subjects by admins")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void postSubject_permitsAdmins() {
+        givenAuthenticatedAs("admin", "admin")
+                .contentType(ContentType.JSON)
+                .body(new CreateSubjectRequest())
+                .log().ifValidationFails()
+                .post("/subject")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("DELETE /subject/<id> permits deleting subjects by admins")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void deleteSubject_permitsAdmins() {
+        // Given subject of id 1 exists
+        var subject = subjectRepository.findById(1L).orElseThrow();
+
+        givenAuthenticatedAs("admin", "admin")
+                .contentType(ContentType.JSON)
+                .log().ifValidationFails()
+                .delete("/subject/"+subject.getId())
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("PATCH /subject/<id> permits admins to change subject details")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void patchSubject_permitsAdmins() {
+        // Given subject of id 1 exists
+        var subject = subjectRepository.findById(1L).orElseThrow();
+
+        givenAuthenticatedAs("admin", "admin")
+                .contentType(ContentType.JSON)
+                .body(new UpdateSubjectRequest())
+                .log().ifValidationFails()
+                .patch("/subject/"+subject.getId())
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("PATCH /subject/<id> permits admins to change subject details")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void patchSubject_forbidsTeachersFromNameUpdate() {
+        // Given subject of id 1 exists
+        var subject = subjectRepository.findById(1L).orElseThrow();
+        var request = new UpdateSubjectRequest();
+
+        // TODO: Refactor UpdateSubjectRequest into record
+        // request.setName("");
+
+        givenAuthenticatedAs("charlesangelica", "secret")
+                .contentType(ContentType.JSON)
+                .body(request)
+                .log().ifValidationFails()
+                .patch("/subject/"+subject.getId())
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("POST /subject/<id>/account allows teachers to add participants")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void postSubjectAccount_teachersCanAddParticipants() {
+        var subject = subjectRepository.findById(1L).orElseThrow();
+        var targetParticipant = accountRepository.findById(2L).orElseThrow();
+
+
+        givenAuthenticatedAs("charlesangelica", "secret")
+                .contentType(ContentType.JSON)
+                .body(List.of(targetParticipant.getId()))
+                .log().ifValidationFails()
+                .post("/subject/%d/account".formatted(subject.getId()))
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("DELETE /subject/<id>/account allows teachers to remove participants")
+    @Sql("classpath:/fixtures/sql/mock_data.sql")
+    void deleteSubjectAccount_teachersCanRemoveParticipants() {
+        var subject = subjectRepository.findById(1L).orElseThrow();
+        var targetParticipant = accountRepository.findById(1L).orElseThrow();
+
+        givenAuthenticatedAs("charlesangelica", "secret")
+                .contentType(ContentType.JSON)
+                .log().ifValidationFails()
+                .delete("/subject/%d/account/%d".formatted(subject.getId(), targetParticipant.getId()))
+                .then()
+                .log().ifValidationFails()
+                .statusCode(204);
+    }
+
+
+}
